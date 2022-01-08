d28f4d6c0471f15965b17f1e015f4834c7f49e46
Adam <adam_fairbrother@live.co.uk>
2022-01-08 21:31:00 +0000

    Updated Content

--- script.js
+++ script.js
@@ -46,5 +46,42 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+}
+
+/*// Arden Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("ardenImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Essential Pages Modal
+
+
+// Get the modal
+modal = document.getElementById("essPagesModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+img = document.getElementById("essPagesImg");
+modalImg = document.getElementById("img02");
+captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}*/
